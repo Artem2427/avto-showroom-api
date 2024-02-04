0de8538e00193eabec6d57b33a211cef85ecc5dc
Artem Danko <vpeta4141@gmail.com>
2024-02-04 09:54:07 +0200

    feat: update randomCode function

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -3,7 +3,6 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 // import * as FormData from 'form-data';
 import FormData from "form-data"
-import { randomCode } from 'src/core/utils/random-code';
 @Injectable()
 export class FileService {
   constructor(private readonly httpService: HttpService) {}
@@ -15,7 +14,7 @@ export class FileService {
     try {
       if (file.mimetype.includes('image')) {
         const formData = new FormData();
-        formData.append('image', file.buffer, file.filename + randomCode(5));
+        formData.append('image', file.buffer, file.filename + this.randomCode(5));
 
         const uploadFile = await this.httpService.axiosRef.post(link, formData);
         return uploadFile.data.data.display_url;
@@ -24,5 +23,18 @@ export class FileService {
     } catch (error) {
       throw new BadRequestException('Incorrect image type');
     }
+  }
+
+  private randomCode(len: number): string {
+    var code = '';
+
+    for (let i = 0; i < len; i++) {
+      var randomNumber = (Math.random() * 10) << 0;
+      code += String.fromCharCode(
+        (randomNumber += randomNumber > 9 ? (randomNumber < 36 ? 55 : 61) : 48),
+      );
+    }
+
+    return code;
   }
 }
